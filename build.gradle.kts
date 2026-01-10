034db4b975dc2f3245e92d2118097d290fd962c2
Adam Korynta <adam@rmanet.com>
2026-01-10 15:51:09 -0800

    add build system support

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.geiconsultants"
-version = "1.0-SNAPSHOT"
+version = "1.0.0-beta1"
 
 repositories {
     mavenCentral()
@@ -32,5 +32,17 @@ tasks {
     patchPluginXml {
         sinceBuild.set("243")
         untilBuild.set("253.*")
+    }
+
+    publishPlugin {
+        val versionStr = project.version.toString()
+        // Regex to find letters after a hyphen (e.g., 'beta' from '1.0.0-beta1')
+        val channelMatch = Regex("-([a-z]+)", RegexOption.IGNORE_CASE).find(versionStr)
+        if (channelMatch != null) {
+            val channelName = channelMatch.groupValues[1].lowercase()
+            channels.set(listOf(channelName))
+        } else {
+            channels.set(listOf("default"))
+        }
     }
 }
